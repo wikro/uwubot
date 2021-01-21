b1faad3811940c59547659bb7ce1614802989a19
Robert Wiklund <robert@wikro.org>
2021-01-21 12:00:52 +0100

    Substational optimization

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const { env } = require("process");
+import { env, exit } from "process";
+import { get } from "https";
+import { Client, MessageEmbed } from "discord.js";
 
 if (!env.hasOwnProperty("SECRET") || env.SECRET.length <= 0) {
-
   console.error("No or zero length SECRET environmental variable");
-
-  return false;
+  exit(1);
 }
 
-// const https = require("https");
-
-
 // ### Tools
-tools = {};
+// Tools are functions used by the client code to parse or fetch data which the client code can then pass to command functions
+const tools = {};
 
-/* tools.fetchUrl = (url) => {
+tools.fetchUrl = (url) => {
 
   return new Promise((resolve, reject) => {
 
-    https.get(url, (res) => {
+    get(url, (res) => {
 
       let data = "";
 
@@ -25,121 +23,78 @@ tools = {};
       res.on("end", () => resolve(data || ""));
 
     })
-    .on("error", (err) => {
-      reject(err);
-    });
+    .on("error", (err) => reject(err));
 
   });
-}; */
+};
 
 tools.parseMessage = (message) => {
 
-  // "!cmd example params" => { cmd: "cmd", params: [ "example", "params"] }
-  const re = message.content.match(/^\!([^ ]+) (.*)$/i) || [];
-  const cmd = re[1] || "";
-  const params = (re[2] || "").trim();
+  // "!cmd example some trailing text" => { cmd: "cmd", trailing: "some trailing text" }
+  const [full, cmd, trailing] = message.content.match(/^\!([^ ]+) \s*(.*?)\s*$/i) || ["", ""];
 
-  return { cmd, params };
+  return { cmd, trailing };
 };
 
-tools.parseAlarm = (timestring) => {
+tools.parseAlarm = (trailing) => {
 
   const time = new Date();
 
-  // "22:37"      => ["22:37", "22", "37", undefined, undefined]
-  // "10:37:55pm" => ["10:37:55 pm", "10", "37", "55", "pm"]
-  const parsed = (timestring.match(/^([0-1]?[0-9]|2[0-4])[.:]?([0-5][0-9])[.:]?([0-5][0-9])?\s*([ap]m)?$/i) || []).map((a) => Number(a) || a );
+  // "22:37" => ["22:37", "22", "37", undefined, undefined], "10:37:55pm" => ["10:37:55 pm", "10", "37", "55", "pm"]
+  const [hours, minutes, seconds, period] = (trailing.match(/^([0-1]?[0-9]|2[0-4])[.:]?([0-5][0-9])[.:]?([0-5][0-9])?\s*([ap]m)?$/i) || []).map((a) => Number(a) || a );
 
-  parsed[1] && end.setHours(parsed[1]);
-  parsed[2] && end.setMinutes(parsed[2]);
-  parsed[3] && end.setSeconds(parsed[3]);
+  hours && end.setHours(hours);
+  minutes && end.setMinutes(minutes);
+  seconds && end.setSeconds(seconds);
 
-  const period = parsed[4] || "24h";
-
-  if (end.getHours() < 13 && format === "pm") {
-    end.setHours(12 + end.getHours());
-  }
+  if (end.getHours() < 13 && format === "pm") end.setHours(12 + end.getHours());
 
   return { time, period };
 };
 
-tools.parseTimer = (timestring) => {
+tools.parseTimer = (trailing) => {
 
-  const start = new Date();
-  const end = new Date(start);
+  const units = { h: "hour", m: "minute", s: "second" };
+  // "1h" => [1, "h"], "5m" => [5, "m"], "60s" => [60, "s"]
+  const [full, time, suffix] = (trailing.match(/^(\d+)\s*([hms])?$/i) || []).map((t) => Number(t) || t);
 
-  // "1h"  => ["1", "h"]
-  // "5m"  => ["5", "m"]
-  // "60s" => ["60", "s"]
-  const parsed = (timestring.match(/^([+-]?\d+)\s*([hms])?$/i) || []).map((t) => Number(t) || t);
+  let timeout = time * 1000
+  if (suffix === "h") timeout = timeout * 60;
+  if (suffix === "h" || suffix === "m") timeout = timeout * 60;
 
-  const time = parsed[1] || 0;
-  const suffix = parsed[2] || "s";
+  let timestring = `${time} ${units[suffix] || units["s"]}`;
+  if (time > 1 || time === 0) timestring += "s";
 
-  switch (suffix) {
-
-    case "h":
-      end.setHours(time + end.getHours());
-      break;
-    case "m":
-      end.setMinutes(time + end.getMinutes());
-      break;
-    default:
-      end.setSeconds(time + end.getSeconds());
-  }
-
-  const hms = { h: "hour", m: "minute", s: "second" };
-  const string = time > 1 ? `${time} ${hms[suffix]}s` : `${time} ${hms[suffix]}`;
-
-  const timeout = end - start;
-
-  return { start, end, string, timeout };
+  return { timestring, timeout };
 };
 
+// ### Commands
+// Commands are functions used by the client code that interact with the users or discord based on the data passed to them
+const commands = {};
 
-// ### Actions
-const actions = {};
+commands.setTimer = ({ message, timestring, timeout }) => {
 
-actions.reply = (message) => {
+  if (timestring && timeout >= 0 && timeout <= 24 * 60 * 60 * 1000) {
+    setTimeout(() => message.channel.send(`Ding ding! ${timestring} has passed!`, { reply: message.author }), timeout);
+
+    return message.channel.send(`Timer goes off in ${timestring}`, { reply: message.author });
+  }
+
+  return message.channel.send(`Usage: \`!t <number>[h | m | s]\`\nExample: '\`!t 20s\`' to start a 20 second timer\nTimers can not be set for more than 24 hours`, { reply: message.author });  
+};
+
+// ### Client code
+// Client code is the wiring between discord.js and the bot, i.e. defining functions for events from Discord
+const client = new Client();
+
+client.on("ready", () => console.info("Discord client ready"));
+
+client.on("message", (message) => {
 
   if (message.author.id === client.user.id) return false;
 
-  const { cmd, params } = tools.parseMessage(message);
-
-  if (cmd === "t" && params.length > 0) {
-
-    const { start, end, string, timeout } = tools.parseTimer(params);
-
-    if (start && end && string && timeout >= 0) {
-      setTimeout(() => {
-
-        message.channel.send(`Ding ding! ${string} has passed!`, { reply: message.author });
-      }, timeout);
-      
-      return message.channel.send(`Sure thing! Your timer goes off in ${string}`, { reply: message.author });
-    }
-
-    return message.channel.send(`Usage: \`!t <number>[h | m | s]\`\nExample: '\`!t 20s\`' to add a 20 second timer`, { reply: message.author });  
-  }
-
-  return false;
-};
-
-
-// ### Discord client code and wiring
-const { Client, MessageEmbed } = require("discord.js");
-
-const client = new Client();
-
-client.on("ready", () => {
-
-  console.info("Discord client ready");
-
-  return true;
-});
-
-client.on("message", (message) => {
-  return actions.reply(message);
+  const { cmd, trailing } = tools.parseMessage(message);
+  if (cmd === "t" && trailing.length > 0) return commands.setTimer({ message, ...tools.parseTimer(trailing) });
 });
 
 client.login(`${env.SECRET}`);
